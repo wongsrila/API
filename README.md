7fdfaf029ab2b4bc6f5504e1ee0a81993063d5e0
wongsrila <jiraphong.wongsrila@gmail.com>
2024-04-17 20:03:58 +0200

    added server sent event - testing

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. Progressive Web App
 2. Share function
 3. Server sent events
+4. Live fixture page
+5. picture in picture
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,25 @@
-// const eventSource = new EventSource('/events');
+// Format timestamp
+function formatTimestampAsTime(timestamp) {
+  // Convert the Unix timestamp from seconds to milliseconds
+  const date = new Date(timestamp * 1000);
+  const timeOptions = {
+    hour: '2-digit',
+    minute: '2-digit',
+  };
+  return date.toLocaleTimeString('nl-NL', timeOptions);
+}
 
-// eventSource.onmessage = function (event) {
-//   document.getElementById('events').innerHTML +=
-//     'New event: ' + event.data + '<br>';
-// };
+const evtSource = new EventSource('/events');
+evtSource.onmessage = function (event) {
+  const data = JSON.parse(event.data);
 
-// eventSource.onerror = function (event) {
-//   console.error('EventSource failed');
-// };
+  const homeScore = data.goals.home;
+  const awayScore = data.goals.away;
+  const time = formatTimestampAsTime(data.fixture.timestamp);
+
+  console.log(time);
+
+  document.getElementById('home-score').innerHTML = homeScore;
+  document.getElementById('away-score').innerHTML = awayScore;
+  document.getElementById('timestamp').innerHTML = time;
+};
